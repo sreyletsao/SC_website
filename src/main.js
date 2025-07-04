009fbe3e65d6f0461552a675a9441903cdf56442
sreyletsao <sreylet.sao@student.passerellesnumeriques.org>
2025-07-04 14:24:47 +0700

    merge code

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'keen-slider/keen-slider.min.css'
+// import 'keen-slider/keen-slider.min.css'
 
 
 const app = createApp(App)
