49d3f41752570de8b86b62e4d5f98ea6d47ce8d5
That-VEN <thattven@gmail.com>
2025-07-04 14:39:59 +0700

    remove

--- src/views/DonateView.vue
+++ src/views/DonateView.vue
@@ -1,11 +1,8 @@
 <template>
-     <TopBar/>
-    <Navbar />
     <Banner/>
     <Header/>
     <Section/>
     <Form/>
-    <Footer/>
 </template>
 
 <script setup lang="ts">
@@ -13,9 +10,6 @@ import Banner from '@/components/donate/Banner.vue';
 import Form from '@/components/donate/Form.vue';
 import Header from '@/components/donate/Header.vue';
 import Section from '@/components/donate/Section.vue';
-import Footer from '@/components/Footer.vue';
-import Navbar from '@/components/Navbar.vue';
-import TopBar from '@/components/TopBar.vue';
 
 </script>
 
